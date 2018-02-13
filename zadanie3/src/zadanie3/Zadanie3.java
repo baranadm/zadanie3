2b114dd7196b761e6ea30204207e10bcb8783fd7
baranadm <baranadm@gmail.com>
2018-02-13 16:48:33 +0100

    zrobione

--- zadanie3/src/zadanie3/Zadanie3.java
+++ zadanie3/src/zadanie3/Zadanie3.java
@@ -28,14 +28,26 @@ public class Zadanie3 {
 			}
 		}
 		System.out.println("-----------");
+		
+		int suma = 0;
+		int licznik = 0;
+		int licznikParzystych = 0;
+		
 		for (int i = 0; i < liczby1.length; i++) {
 			if (istnieje(liczby1[i], liczby2)) {
 				System.out.println("Powtarza sie: " + liczby1[i]);
+				suma = suma + liczby1[i];
+				licznik++;
+				if (liczby1[i]%2==0) licznikParzystych++;
 			}
 		}
+		
 		// wypisz sume powtarzajacych sie liczb
+		System.out.println("Suma powtarzajacych sie liczb: " + suma);
 		// wypisz ilosc powtarzajacych sie liczb
+		System.out.println("Ilosc powtorzen liczb: " + licznik);
 		// wypisz ilosc parzystych powtarzajacych sie liczb
+		System.out.println("Ilosc powtorzen liczb parzystych: " + licznikParzystych);
 		
 	
 		
